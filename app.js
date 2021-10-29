f4fe90853bf769baf2025c5987848f542e47286b
Laure <quanelle@gmail.com>
2021-10-29 19:30:41 +0200

    résolution bug
    user vers User
    chgmts routes

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
+"use strict";
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 
 const sauceRoute = require('./routes/SauceRoute');
 const userRoute = require('./routes/UserRoute');
 
-const app = express(); 
 
 mongoose.connect('mongodb+srv://OCLP6:testmdp@clusterp6.82nvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-   
-    res.setHeader('Access-Control-Allow-Origin', '*');    
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 //  transforme données en json
-  app.use(express.json());
+app.use(express.json());
 
-  app.use('/api/auth', userRoute);
 
-  app.use('/api/sauces', sauceRoute);
 
-  
-// export de l'app pour qu'autre fichiers du projet puissent y accéder dt serveur node
+app.use('/api/auth', userRoute);
+
+app.use('/api/sauces', sauceRoute);
+
+
+
+// export de l'app pour qu'autres fichiers du projet puissent y accéder dt serveur node
 module.exports = app;
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,24 @@
+"use strict";
 const user = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 
-exports.signup =(req, res, next)=>{
-    bcrypt.hash(req.body.password, 10)
-    .then(hash=>{
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        })
-        user.save()
-        .then((res.status(201).json({message:'utilisateur créé'})))
-        .catch(error=>res.status(400).json({error}));
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      })
+      user.save()
+        .then((res.status(201).json({ message: 'utilisateur créé' })))
+        .catch(error => res.status(400).json({ error }));
     })
-    .catch(error=>res.status(500).json({error}));
+    .catch(error => res.status(500).json({ error }));
 
 };
 
 exports.login = (req, res, next) => {
-    user.findOne({ email: req.body.email })
+  user.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé' });
@@ -25,7 +26,7 @@ exports.login = (req, res, next) => {
       bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect'});
+            return res.status(401).json({ error: 'Mot de passe incorrect' });
           }
           res.status(200).json({
             userId: user._id,
